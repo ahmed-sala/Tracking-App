0255db1a0c7ea3b269665f4a95fbcff21f734ca5
Ahmed Salah Aldin Ahmed <92693085+ahmed-sala@users.noreply.github.com>
2025-01-29 19:33:45 +0200

    solve errors from merging

--- lib/src/presentation/managers/order/pending_order/pending_order_event.dart
+++ lib/src/presentation/managers/order/pending_order/pending_order_event.dart
@@ -1,6 +1,7 @@
 sealed class PendingOrderEvent {}
-<<<<<<< HEAD
+
 class GetAllPendingOrderEvent extends PendingOrderEvent {}
+
 class RejectOrderEvent extends PendingOrderEvent {
   final String orderId;
   RejectOrderEvent(this.orderId);
--- lib/src/presentation/managers/order/pending_order/pending_order_state.dart
+++ lib/src/presentation/managers/order/pending_order/pending_order_state.dart
@@ -1,14 +1,13 @@
 part of 'pending_order_cubit.dart';
 
-<<<<<<< HEAD
-=======
-@immutable
->>>>>>> origin/feature_pending_order
 sealed class PendingOrderState {}
 
 final class PendingOrderInitial extends PendingOrderState {}
+
 class GetAllPendingOrderLoadingState extends PendingOrderState {}
+
 class GetAllPendingOrderSuccessState extends PendingOrderState {}
+
 class GetAllPendingOrderErrorState extends PendingOrderState {
   final Exception exception;
   GetAllPendingOrderErrorState({required this.exception});
